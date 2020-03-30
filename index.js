949e31efd490071030b0a7ba34ad51f2d551495f
mrarthurwhite <mister.arthur.white@gmail.com>
2020-03-29 21:32:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    //console.log("The DOM has loaded");
+    //t= document.getElementById("text");
+    t = document.querySelector( 'p' );
+    t.textContent="This is really cool!";
+});
+   
